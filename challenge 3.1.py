e7f41123abcaa804ea36230211ace741204a2d4e
047AFEE89041D3BB2C1469A6B51F2BB6 <145101910+047AFEE89041D3BB2C1469A6B51F2BB6@users.noreply.github.com>
2023-09-25 12:58:11 +0530

    LinearSearchProduct.py

--- challenge 3.1.py
+++ challenge 3.1.py
@@ -1,22 +1,12 @@
-def linearSearchProduct(productList, targetProduct):
+def linear_search_product(product_list, target_product):
+  indices = []
+  for i, product in enumerate(product_list):
+    if product == target_product:
+      indices.append(i)
+  return indices
 
- indices = []
 
- for index, product in enumerate(productList):
-if product == targetProduct:
-
- indices.append(index)
-
- return indices
-
-# Example usage:
-
-products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
-
-target = "shoes"
-
-target2 = 'apple'
-
-result = linearSearchProduct(products, target)
-
+products = ["pen", "pencil", "pen", "eraser", "scale", "pen"]
+target = "pen"
+result = linear_search_product(products, target)
 print(result)
